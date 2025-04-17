d4ee62ca5a359e1ac6a1faaa470a5aedf33afbbf
Shashvat <shashvat990@gmail.com>
2025-04-17 14:17:06 +0530

    Added user capabilities

--- vihaan_project_gfns/Backend/controllers/userController.js
+++ vihaan_project_gfns/Backend/controllers/userController.js
@@ -1,22 +1,26 @@
 import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 export const registerUser = async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+  const { username, password } = req.body;
+  console.log("received the user")
+
+  const hash = await bcrypt.hash(password, 10);
+  const user = await User.create({ username, password: hash });
+  console.log("create")
+
+  res.json({ username: user.username });
 };
 
 export const loginUser = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  
-  if (!user || user.password !== password) {
-    return res.status(401).json({ message: 'Invalid credentials' });
-  }
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+  if (!user) return res.status(400).json({ message: 'User not found' });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(401).json({ message: 'Wrong password' });
 
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
   res.json({ token });
--- vihaan_project_gfns/Backend/server.js
+++ vihaan_project_gfns/Backend/server.js
@@ -1,14 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import connectDB from './config.js';
-
 import userRoutes from './routes/userRoutes.js';
-import propertyRoutes from './routes/propertyRoutes.js';
-import investmentRoutes from './routes/investmentRoutes.js';
+// import propertyRoutes from './routes/propertyRoutes.js';
+// import investmentRoutes from './routes/investmentRoutes.js';
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
 
 dotenv.config();
-connectDB(); // MongoDB connection
+connectDB();
 
 const app = express();
 app.use(express.json());
@@ -18,10 +31,14 @@ app.use(cors({
   credentials: true
 }));
 
-// Routes
+app.get('/', (req, res) => {
+    res.send('Backend is running ✅');
+  });
+  
+
 app.use('/api/users', userRoutes);
-app.use('/api/properties', propertyRoutes);
-app.use('/api/investments', investmentRoutes);
+// app.use('/api/properties', propertyRoutes);
+// app.use('/api/investments', investmentRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
