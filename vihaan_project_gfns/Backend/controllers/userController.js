caedf526ce96a57b7460c98c1695721cc4404644
Shashvat <shashvat990@gmail.com>
2025-04-17 15:30:45 +0530

    completed login

--- vihaan_project_gfns/Backend/controllers/userController.js
+++ vihaan_project_gfns/Backend/controllers/userController.js
@@ -21,6 +21,7 @@ export const loginUser = async (req, res) => {
 
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) return res.status(401).json({ message: 'Wrong password' });
+  console.log("user_created")
 
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
   res.json({ token });
--- vihaan_project_gfns/Backend/models/User.js
+++ vihaan_project_gfns/Backend/models/User.js
@@ -1,13 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const userSchema = new mongoose.Schema({
-//   name: String,
-//   email: { type: String, unique: true },
-//   password: String,
-//   investments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Investment' }]
-// });
-
-// export default mongoose.model('User', userSchema);
 
 import mongoose from 'mongoose';
 
